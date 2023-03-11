ef434a1ea6908ed6c08bd3ce4ae1f496e44f82c9
syf <2654525303@qq.com>
2023-03-12 07:26:17 +0800

    set routing for standalone

---
+++ src/app/software-engineer/software-engineer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-software-engineer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './software-engineer.component.html',
+  styleUrls: ['./software-engineer.component.scss']
+})
+export class SoftwareEngineerComponent {
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,23 @@
 import {bootstrapApplication} from "@angular/platform-browser";
 import {AppComponent} from "./app/app.component";
+import {provideRouter, Routes} from "@angular/router";
 
-bootstrapApplication(AppComponent)
+const routes: Routes = [
+  {
+    path: 'game-dev',
+    loadComponent: () => import("./app/game-dev/game-dev.component")
+      .then(mod => mod.GameDevComponent)
+  },
+  {
+    path: 'software-engineer',
+    loadComponent: () => import("./app/software-engineer/software-engineer.component")
+      .then(mod => mod.SoftwareEngineerComponent)
+  }
+];
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes)
+  ]
+})
   .then(_ => console.log("App has launched successfully!"))
   .catch(reason => console.log(reason));
