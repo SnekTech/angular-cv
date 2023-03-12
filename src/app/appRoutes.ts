8c943c08706e7cdcf0ec9581cc5da78489725266
syf <2654525303@qq.com>
2023-03-13 00:31:51 +0800

    refactor folder structure

--- src/app/appRoutes.ts
+++ src/app/appRoutes.ts
@@ -3,12 +3,12 @@ import {Routes} from "@angular/router";
 export const appRoutes: Routes = [
   {
     path: 'game-dev',
-    loadComponent: () => import("./game-dev/game-dev.component")
+    loadComponent: () => import("./cv/game-dev/game-dev.component")
       .then(mod => mod.GameDevComponent)
   },
   {
     path: 'software-engineer',
-    loadComponent: () => import("./software-engineer/software-engineer.component")
+    loadComponent: () => import("./cv/software-engineer/software-engineer.component")
       .then(mod => mod.SoftwareEngineerComponent)
   }
 ];
