fe95532b8f14fa5407376d2d565b6b290b368ca0
syf <2654525303@qq.com>
2023-03-12 21:44:32 +0800

    header-sidebar-content layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from "@angular/router";
+import {SidebarComponent} from "./sidebar/sidebar.component";
 
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    SidebarComponent,
   ],
   styleUrls: ['./app.component.scss']
 })
