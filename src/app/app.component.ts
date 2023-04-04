9019079e66bd860071b64ea24551ca568c344b24
syf <2654525303@qq.com>
2023-04-04 23:26:20 +0800

    toggle print mode using ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from "@angular/router";
 import {SidebarComponent} from "./sidebar/sidebar.component";
+import {CommonModule} from "@angular/common";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   standalone: true,
@@ -9,9 +11,13 @@ import {SidebarComponent} from "./sidebar/sidebar.component";
   imports: [
     RouterOutlet,
     SidebarComponent,
+    CommonModule,
+    MatButtonModule,
   ],
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'angular-cv';
+
+  isPrintMode = false;
 }
