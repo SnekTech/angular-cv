66efc699d8398bf8da5b5145254fa14af7a29e4c
syf <2654525303@qq.com>
2023-04-04 23:08:17 +0800

    add angular material

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import {bootstrapApplication} from "@angular/platform-browser";
 import {provideRouter} from "@angular/router";
 import {AppComponent} from "./app/app.component";
 import {appRoutes} from "./app/appRoutes";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
 
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(appRoutes)
-  ]
+    provideRouter(appRoutes),
+    importProvidersFrom(BrowserAnimationsModule)
+]
 })
   .then(_ => console.log("App has launched successfully!"))
   .catch(reason => console.log(reason));
